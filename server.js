f30740a0f3f1f4a3bcd4971d5bb14cc4e5b28322
Yago Pérez Molanes <alu0101254678@ull.edu.es>
2023-06-08 05:51:08 +0100

    Added delete route

--- server.js
+++ server.js
@@ -156,7 +156,7 @@ app.delete('/paths/:id', async (req, res) => {
     res.status(200).send('Ruta eliminada exitosamente');
   } catch (error) {
     console.error(error);
-    return res.status(500).json({error: error.message});
+    return res.status(500).json({error: 'hola'});
   }
 });
 
