dea9ffca7642f0c48cbbbc92d19c49e594e985f4
Yago Pérez Molanes <alu0101254678@ull.edu.es>
2023-07-05 18:18:47 +0100

    Changed README to add resource of application url

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# TFG-2023-YagoPerezMolanes-Backend
+# Trip Tracking TT
 
 [![Node.js CI](https://github.com/alu0101254678/TFG-2023-YagoPerezMolanes-Backend/actions/workflows/node.js.yml/badge.svg)](https://github.com/alu0101254678/TFG-2023-YagoPerezMolanes-Backend/actions/workflows/node.js.yml)
+
+## URLS del proyecto
+
+* [https://trick-tracking-app-backend.herokuapp.com/]( https://trick-tracking-app-backend.herokuapp.com/)
--- database.js
+++ database.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose");
+/* eslint-disable linebreak-style */
+const mongoose = require('mongoose');
 
 // mongoose es la libreria que permite conectarnos a una base de datos de
 // mongodb, y permite modelar los datos, aqui lo usamos para conectarnos
 // a la base de datos de mongodb, alojada en la nube(mongodb atlas)
 
 // se llama a una funcion que devuleve una promesa
-mongoose.connect("mongodb+srv://alu0101254678:Z8aKR8cP@cluster0.6wih6s4.mongodb.net/?retryWrites=true&w=majority",
+mongoose.connect('mongodb+srv://alu0101254678:Z8aKR8cP@cluster0.6wih6s4.mongodb.net/?retryWrites=true&w=majority',
     {useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then((db) => console.log("Database is connected"))
+    .then((db) => console.log('Database is connected'))
     .catch((err) => console.log(err));
